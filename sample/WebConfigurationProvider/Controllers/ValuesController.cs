3666924875eaef7a65eaa30a7bbe0767fc56a334
andrewlock <sock@andrewlock.net>
2016-10-13 12:40:03 +0100

    Add web configuration provider project

---
+++ sample/WebConfigurationProvider/Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebConfigurationProvider.Controllers
+{
+    [Route("api/[controller]")]
+    public class ConfigurationController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public Dictionary<string, string> Get()
+        {
+            return new Dictionary<string, string>
+            {
+                { "remote:inner1:value1", "thevalue" },
+                { "remote:inner1:value2", "2nd value" },
+                { "remote:value1", "1" },
+                { "remote:value2", "2" },
+            };
+        }
+    }
+}
