644d5ac9772527146915480da22e9102d6afcf37
Andrew Lock <andrewlock.net@gmail.com>
2017-09-07 09:03:32 +0100

    Add 2.0 demo project

---
+++ sample/WebDemoProject2_0/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NetEscapades.Configuration.Remote;
+
+namespace WebDemoProject2_0
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration(AddConfiguration)
+                .UseStartup<Startup>()
+                .Build();
+
+        private static void AddConfiguration(IConfigurationBuilder builder)
+        {
+
+            builder
+                .AddYamlFile("appsettings.yml", optional: false)
+                .AddRemoteSource(new Uri("http://localhost:5001/api/configuration"))
+                .AddRemoteSource(new Uri("http://localhost:5001/api/endpoint/does/not/exist"), optional: true)
+                .AddRemoteSource(new Uri("http://localhost:5002/host/does/not/exist"), optional: true)
+                .AddRemoteSource(new RemoteConfigurationSource()
+                {
+                    ConfigurationUri = new Uri("http://localhost:5001/api/configuration"),
+                    ConfigurationKeyPrefix = "anextraprefix",
+                    Events = new RemoteConfigurationEvents
+                    {
+                        OnDataParsed = dict =>
+                        {
+                            var result = new SortedDictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+                            foreach (var keyValuePair in dict)
+                            {
+                                if (!string.IsNullOrEmpty(keyValuePair.Value))
+                                {
+                                    result[keyValuePair.Key] = keyValuePair.Value + " is augemented!";
+                                }
+                            }
+                            return result;
+                        }
+                    }
+                });
+        }
+    }
+}
