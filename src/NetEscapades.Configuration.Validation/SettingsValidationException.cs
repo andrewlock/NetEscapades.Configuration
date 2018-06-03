b541da36aa0d1859741647fbfa34eae1115e1707
Andrew Lock <andrewlock.net@gmail.com>
2018-06-03 19:39:52 +1200

    Added initial code for configuration validation using IStartupFilter

---
+++ src/NetEscapades.Configuration.Validation/SettingsValidationException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NetEscapades.Configuration.Validation
+{
+    /// <summary>
+    /// A utility <see cref="Exception"/> that indicates a strong typed configuration model was not configured correctly
+    /// </summary>
+    public class SettingsValidationException : Exception
+    {
+        /// <summary>
+        /// Creates a new instance of the <see cref="SettingsValidationException"/>
+        /// </summary>
+        /// <param name="className">The name of the class being validated</param>
+        /// <param name="propertyName">The property of the instance that was invalid</param>
+        /// <param name="error">A description of the configuration error</param>
+        public SettingsValidationException(string className, string propertyName, string error)
+            : this(GetMessage(className, propertyName, error))
+        {
+        }
+
+        private static string GetMessage(string className, string propertyName, string message)
+        {
+            return $@"Settings were invalid: {className}.{propertyName} {message}. 
+
+Check that your configuration has been loaded correctly, and all necessary values are set in the configuration files.";
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="SettingsValidationException"/>
+        /// </summary>
+        /// <param name="errorMessage">A description of the configuration error</param>
+        public SettingsValidationException(string errorMessage) : base(errorMessage)
+        {
+        }
+    }
+}
---
+++ test/NetEscapades.Configuration.Validation.Tests/StronglyTypedModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NetEscapades.Configuration.Validation.Tests
+{
+    public class StronglyTypedModel : IValidatable
+    {
+        public string Url { get; set; }
+        public int Port { get; set; }
+
+        public void Validate()
+        {
+            if (string.IsNullOrWhiteSpace(Url))
+            {
+                throw new SettingsValidationException(nameof(StronglyTypedModel), nameof(Url), "Must not be null or whitespace");
+            }
+
+            // will throw if invalid Url
+            var uri = new Uri(Url);
+
+            if(Port <= 0)
+            {
+                throw new SettingsValidationException(nameof(StronglyTypedModel), nameof(Port), "Must be greater than 0");
+            }
+        }
+    }
+}
