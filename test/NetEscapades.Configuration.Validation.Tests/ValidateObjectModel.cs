a44ab313c232257790460631c3d370279c42dd58
Andrew Lock <andrewlock.net@gmail.com>
2018-07-10 13:45:16 +0700

    Add tests for validation using DataAnnotationsAttributes

---
+++ test/NetEscapades.Configuration.Validation.Tests/ValidateObjectModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NetEscapades.Configuration.Validation.Tests
+{
+    public class ValidateObjectModel : IValidatable
+    {
+        [Required(AllowEmptyStrings = false), Url]
+        public string Url { get; set; }
+
+        [Range(minimum: 1, maximum: 65535)]
+        public int Port { get; set; }
+
+        public void Validate()
+        {
+            Validator.ValidateObject(this, new ValidationContext(this), validateAllProperties: true);
+        }
+    }
+}
