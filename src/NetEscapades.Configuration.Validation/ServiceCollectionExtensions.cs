3d82814c936fff3123362faa1dc3278631cb5d25
Andrew Lock <andrewlock.net@gmail.com>
2020-01-03 17:03:09 +0000

    Add support for .NET Core 3.0 to NetEscapades.Configuration.Validation
    Switch to using IHostedService instead of IStartupFilter for .NET Core 3.0 apps

--- src/NetEscapades.Configuration.Validation/ServiceCollectionExtensions.cs
+++ src/NetEscapades.Configuration.Validation/ServiceCollectionExtensions.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿#if NETSTANDARD2_1
+using Microsoft.Extensions.Hosting;
+#else
+using Microsoft.AspNetCore.Hosting;
+#endif
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Options;
 using NetEscapades.Configuration.Validation;
@@ -7,6 +11,15 @@ namespace Microsoft.Extensions.DependencyInjection
 {
     public static class ServiceCollectionExtensions
     {
+#if NETSTANDARD2_1
+        /// <summary>
+        /// Add an <see cref="IHostedService"/> to the application that invokes <see cref="IValidatable.Validate"/> on all registered objects
+        /// </summary>
+        public static IServiceCollection UseConfigurationValidation(this IServiceCollection services)
+        {
+            return services.AddTransient<IHostedService, SettingValidationHostedService>();
+        }
+#else
         /// <summary>
         /// Add an <see cref="IStartupFilter"/> to the application that invokes <see cref="IValidatable.Validate"/> on all registered objects
         /// </summary>
@@ -14,6 +27,7 @@ namespace Microsoft.Extensions.DependencyInjection
         {
             return services.AddTransient<IStartupFilter, SettingValidationStartupFilter>();
         }
+#endif
 
         /// <summary>
         /// Registers a configuration instance which <typeparamref name="TOptions"/> will bind against, and registers as a validatble setting. 
