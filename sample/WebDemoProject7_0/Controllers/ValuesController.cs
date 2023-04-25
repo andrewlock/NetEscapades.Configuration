f7eec647bb3044696623e381a8601bdca9875162
Andrew Lock <andrewlock.net@gmail.com>
2023-04-25 23:02:54 +0100

    Add a .NET 7 sample that uses AddYamlStream

---
+++ sample/WebDemoProject7_0/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace WebDemoProject2_0.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly IConfiguration _config;
+
+        public ValuesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+             return _config.AsEnumerable()
+             .OrderBy(kvp => kvp.Key)
+             .Select(kvp => kvp.Key + " - " + kvp.Value);
+        }
+    }
+}
